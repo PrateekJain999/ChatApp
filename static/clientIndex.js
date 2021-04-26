8981fd12acb37a2dddf052eb79a98e306adfe9a8
Prateek Jain <jain22719@gmail.com>
2021-04-26 18:14:30 +0530

    css added

--- static/clientIndex.js
+++ static/clientIndex.js
@@ -2,16 +2,8 @@ let socket = io();
 
 var name = window.prompt("Enter your name: ");
 
-socket.on('connect', function(){
-    console.log("User connected");
-});
-
-socket.on('disconnect', function(){
-    console.log("User disconnected");
-});
 
 socket.on('newMessage', function(message){
-    console.log(message);
     const formattedTime = moment(message.createdAt).format('LT');
     let li = document.createElement('li');
 
